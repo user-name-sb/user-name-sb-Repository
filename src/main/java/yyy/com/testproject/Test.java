58eb16ea1a1908170f27fb74423ffc32d787710e
lexed <lexed@BD-02-22>
2020-09-28 15:21:34 +0800

    1.1.2 yyy

--- src/main/java/yyy/com/testproject/Test.java
+++ src/main/java/yyy/com/testproject/Test.java
@@ -3,5 +3,8 @@ package yyy.com.testproject;
 public class Test {
 	public static void main(String asgs[]){
 		System.out.println("Hello Git");
+		System.out.println("À±¹·×ÓÀ± À±¹·×ÓÀ±");
+		System.out.println("À±¹·×ÓÏãÑ½¹ş ºô¹şÀ±£¡");
+		
 	}
 }
